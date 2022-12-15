ad8b60e7be75646f98f9fbfb40daa9878893f32a
LotarMK <m.kwijas@interia.pl>
2022-12-15 15:04:18 +0100

    szyfr cezara

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String text = "ABC";
+        int key = 1;
+        System.out.println(text);
+        System.out.println("\uD83D\uDC27" + "-----------------" + "\uD83D\uDC27");
+
+        StringBuilder sb = new StringBuilder(); //dużo szybszy od konkatenacji
+//        for(int i = 0; i < text.length(); ++i) {
+//            int c = text.charAt(i);
+//            c += key;
+//            sb.append((char)c);
+//        }
+
+        text.chars()
+                .mapToObj(c -> (char)(c + 1))
+                .forEach(c -> sb.append(c));
+
+        String encText = sb.toString();
+        System.out.println(encText);
     }
 }
